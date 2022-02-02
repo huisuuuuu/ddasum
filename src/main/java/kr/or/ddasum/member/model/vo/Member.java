5c02a83aaffe5ff2df9e6e2bc47ece93e0e8d06d
Jungdoosik <entlrsla@gmail.com>
2022-02-03 00:27:32 +0900

    member객체생성 후 데이터 삽입

--- src/main/java/kr/or/ddasum/member/model/vo/Member.java
+++ src/main/java/kr/or/ddasum/member/model/vo/Member.java
@@ -1,5 +1,23 @@
 package kr.or.ddasum.member.model.vo;
 
+import java.sql.Date;
+
 public class Member {
+	private int userNo;
+	private String authorityId;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String nick;
+	private String email;
+	private char detail;
+	private String address;
+	private String pImage;
+	private Date enrollDate;
+	private int cardNo;
+	private char delYN;
+	
+	
+	
 	
 }
