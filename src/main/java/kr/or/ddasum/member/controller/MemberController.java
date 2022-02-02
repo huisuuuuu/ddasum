7d0b80bedfa3edd5296058ebc1f0a5ebee3a07ea
Jungdoosik <entlrsla@gmail.com>
2022-02-02 15:42:59 +0900

    수정한것입니다.

--- src/main/java/kr/or/ddasum/member/controller/MemberController.java
+++ src/main/java/kr/or/ddasum/member/controller/MemberController.java
@@ -1,12 +1,9 @@
 package kr.or.ddasum.member.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.or.ddasum.member.model.service.MemberService;
 
@@ -18,7 +15,7 @@ public class MemberController {
 	
 	@RequestMapping(value="/member/login.do", method = RequestMethod.POST)
 	public String login() {
-		System.out.println("연습이에요");
+		
 		return "/member/memberLogin";
 		
 	}
